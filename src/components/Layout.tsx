e3910390b93d2797c1a810849bd22eb7a20e15ef
Mario Carrillo <mario.carrillo@clearcheck.us>
2023-01-28 13:49:50 -0600

    moved auth provider to _app.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,29 +1,27 @@
-import { Auth } from '@supabase/auth-ui-react';
+import { SupabaseClient } from '@supabase/supabase-js';
 import Head from 'next/head';
 import { ReactNode } from 'react';
-import supabaseClient from 'utils/supabaseClient';
 import Header from './Header';
 
 type Props = {
    children?: ReactNode;
    title?: string;
+   supabaseClient: SupabaseClient;
 };
 
-const Layout = ({ children, title = 'World Timer' }: Props) => (
+const Layout = ({ children, title = 'World Timer', supabaseClient }: Props) => (
    <div>
       <Head>
          <title>{title}</title>
          <meta charSet="utf-8" />
          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Auth.UserContextProvider supabaseClient={supabaseClient}>
-         <Header supabaseClient={supabaseClient} />
-         {children}
-         <footer>
-            <hr />
-            <span>I'm here to stay (Footer)</span>
-         </footer>
-      </Auth.UserContextProvider>
+      <Header supabaseClient={supabaseClient} />
+      {children}
+      <footer>
+         <hr />
+         <span>I'm here to stay (Footer)</span>
+      </footer>
    </div>
 );
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import { AppProps } from 'next/app';
 
 import Layout from '@components/Layout';
+import { Auth } from '@supabase/auth-ui-react';
+import supabaseClient from 'utils/supabaseClient';
 import '../styles/index.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
    return (
-      <Layout>
-         <Component {...pageProps} />
-      </Layout>
+      <Auth.UserContextProvider supabaseClient={supabaseClient}>
+         <Layout supabaseClient={supabaseClient}>
+            <Component {...pageProps} />
+         </Layout>
+      </Auth.UserContextProvider>
    );
 }
 
